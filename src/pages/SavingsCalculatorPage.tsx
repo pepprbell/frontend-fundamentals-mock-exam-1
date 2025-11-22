80f0da5878fa2f42373661aedb204f533b45ce04
pepprbell <pepprbell@gmail.com>
2025-11-22 22:48:10 +0900

    add: apply inputs & filtering to main page

--- src/pages/SavingsCalculatorPage.tsx
+++ src/pages/SavingsCalculatorPage.tsx
@@ -1,33 +1,21 @@
 import {
-  Assets,
   Border,
-  colors,
-  ListHeader,
-  ListRow,
   NavigationBar,
-  SelectBottomSheet,
   Spacing,
   Tab,
-  TextField,
 } from 'tosslib';
 import SavingsProduct from 'domains/savingsProduct/SavingsProduct';
+import Inputs from 'domains/inputs/Inputs';
 
 export function SavingsCalculatorPage() {
+
   return (
     <>
       <NavigationBar title="적금 계산기" />
 
       <Spacing size={16} />
 
-      <TextField label="목표 금액" placeholder="목표 금액을 입력하세요" suffix="원" />
-      <Spacing size={16} />
-      <TextField label="월 납입액" placeholder="희망 월 납입액을 입력하세요" suffix="원" />
-      <Spacing size={16} />
-      <SelectBottomSheet label="저축 기간" title="저축 기간을 선택해주세요" value={12} onChange={() => {}}>
-        <SelectBottomSheet.Option value={6}>6개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={12}>12개월</SelectBottomSheet.Option>
-        <SelectBottomSheet.Option value={24}>24개월</SelectBottomSheet.Option>
-      </SelectBottomSheet>
+      <Inputs />
 
       <Spacing size={24} />
       <Border height={16} />
