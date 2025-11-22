747fababcde9f20c73fb88cfbf23a1659d9e7cbe
pepprbell <pepprbell@gmail.com>
2025-11-23 00:01:32 +0900

    add: error check for savingProduct data fetching

--- src/domains/savingsProduct/hooks/useSavingsProductData.tsx
+++ src/domains/savingsProduct/hooks/useSavingsProductData.tsx
@@ -1,27 +1,23 @@
 import { useEffect, useState } from "react"
-import { http } from "tosslib";
+import { http, isHttpError } from "tosslib";
 import { SavingsProduct } from "../SavingsProduct";
 
 export default function useSavingsProductData() {
   const [data, setData] = useState<SavingsProduct[]>([])
 
   useEffect(() => {
-    http.get<SavingsProduct[]>(
-      '/api/savings-products',
-    ).then((response) => { // [{"id": 'savings-001', "name": "기본 정기적금", ... }]
-      setData(response)
-    })
+    try {
+      http.get<SavingsProduct[]>(
+        '/api/savings-products',
+      ).then((response) => { // [{"id": 'savings-001', "name": "기본 정기적금", ... }]
+        setData(response)
+      })
+    } catch (e) {
+      if (isHttpError(e)) {
+        console.error(e.message)
+      }
+    }
   }, [])
-
-  // // API 오류 캐치하기
-  // try {
-  //   await http.post(...);
-  // } catch (e) {
-  //   if (isHttpError(e)) {
-  //     console.log(e.message);
-  //   }
-  // }
-
-
+  
   return data
 }
